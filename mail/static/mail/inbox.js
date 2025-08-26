9a3b2ed6042c16db4b38b5e7a76e2aa6a5178238
Innocent Anozie <iygeal@gmail.com>
2025-08-26 23:30:42 +0100

    Added submit event to compose form with send_email function as event listener.

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -1,17 +1,26 @@
-document.addEventListener('DOMContentLoaded', function() {
-
+document.addEventListener('DOMContentLoaded', function () {
   // Use buttons to toggle between views
-  document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
-  document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
-  document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
+  document
+    .querySelector('#inbox')
+    .addEventListener('click', () => load_mailbox('inbox'));
+  document
+    .querySelector('#sent')
+    .addEventListener('click', () => load_mailbox('sent'));
+  document
+    .querySelector('#archived')
+    .addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
+
+  // Handle form submission
+  document
+    .querySelector('#compose-form')
+    .addEventListener('submit', send_email);
 
   // By default, load the inbox
   load_mailbox('inbox');
 });
 
 function compose_email() {
-
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
@@ -23,11 +32,45 @@ function compose_email() {
 }
 
 function load_mailbox(mailbox) {
-  
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
 
   // Show the mailbox name
-  document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+  document.querySelector('#emails-view').innerHTML = `<h3>${
+    mailbox.charAt(0).toUpperCase() + mailbox.slice(1)
+  }</h3>`;
+}
+
+// Logic to send email
+function send_email(event) {
+
+  // Stop form from submitting by default so send_email handles it
+  event.preventDefault();
+
+  // Get the form values
+  const recipients = document.querySelector('#compose-recipients').value;
+  const subject = document.querySelector('#compose-subject').value;
+  const body = document.querySelector('#compose-body').value;
+
+  // Send a POST request with the values
+  fetch('/emails', {
+    method: 'POST',
+    body: JSON.stringify({
+      recipients: recipients,
+      subject: subject,
+      body: body,
+    }),
+  })
+    .then((response) => response.json())
+    .then((result) => {
+      // Log result for debugging
+      console.log(result);
+
+      // Load sent mail box
+      load_mailbox('sent');
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
 }
